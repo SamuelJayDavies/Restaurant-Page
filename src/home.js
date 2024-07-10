46ce419a1789c0f9cbc1f511c762e3f65b969555
Samuel Davies <samj.davies@virginmedia.com>
2024-07-10 19:44:41 +0100

    Added more home page graphics and begin plan for menu

--- src/home.js
+++ src/home.js
@@ -1,19 +1,29 @@
 import './style.css';
-import logo from "./logo.png";
+import universalLogo from "./logo.png";
+import universalCityWalkBanner from "./citywalk-banner.jpg";
 
 function createHomePage() {
     const main = document.getElementById("main");
     main.textContent = "";
     main.appendChild(createHeader());
+    main.appendChild(createContent());
+    main.appendChild(createFooter());
 }
 
 function createHeader() {
     const headerMain = document.createElement('div');
     headerMain.classList.add("main-header");
 
+    const topHeader = document.createElement("div");
+    topHeader.classList.add("top-header");
+
+    const logo = document.createElement("img");
+    logo.src = universalLogo;
+    
     const header = document.createElement("header");
     const nav = document.createElement("nav");
 
+    topHeader.appendChild(logo);
     headerMain.appendChild(header);
 
     const title = document.createElement("h1");
@@ -31,11 +41,46 @@ function createHeader() {
     nav.appendChild(menuBtn);
     nav.appendChild(aboutBtn);
 
-    header.appendChild(title);
+    topHeader.appendChild(title);
+
+    header.appendChild(topHeader);
 
     header.appendChild(nav);
     
     return headerMain;
+}
+
+function createContent() {
+    const mainContentContainer = document.createElement("div");
+    mainContentContainer.classList.add("main-content-container");
+
+    const welcomeDiv = document.createElement("div");
+    welcomeDiv.classList.add("welcome-container");
+    
+    const welcomeHeader = document.createElement("h1");
+    welcomeHeader.textContent = "Welcome to Universal Food Insight";
+
+    const introPara = document.createElement("p");
+    introPara.textContent = "Here you can view the menu of any citywalk restaurant";
+
+    welcomeDiv.appendChild(welcomeHeader);
+    welcomeDiv.appendChild(introPara);
+
+    mainContentContainer.appendChild(welcomeDiv);
+
+    return mainContentContainer;
+}
+
+function createFooter() {
+    const footer = document.createElement("div");
+    footer.classList.add("footer");
+    
+    const footerPara = document.createElement("p");
+    footerPara.textContent = "© Samuel Davies 2024";
+
+    footer.appendChild(footerPara);
+
+    return footer;
 }
 
 export default createHomePage;
